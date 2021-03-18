1b6051f5f38dbdf5f591fac312c33a21490479e9
sashapff <a.ivanova@niuitmo.ru>
2021-03-18 19:10:19 +0300

    Comment code

--- MCMC_orientation/main.py
+++ MCMC_orientation/main.py
@@ -1,38 +1,38 @@
-from load import get_contigs_and_pairs
-from prob import density
-from model import MCMC
-from tools import get_orientation
-import numpy as np
+# from load import get_contigs_and_pairs
+# from prob import density
+# from model import MCMC
+# from tools import get_orientation
+# import numpy as np
 
 
 if __name__ == "__main__":
     print("Start!")
-    # Example
-
-    path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr1.layout.txt"
-    path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/test_data/comp18_lens.tsv"
-    path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs1.txt"
-
-    # longest_contig
-    pairs, contigs, id_contig, longest_contig = get_contigs_and_pairs(path_layout, path_lens, path_pairs, long_contig=True)
-
-    correct_contigs = [contig.o for contig in contigs]
-
-    print("Estimation of density...")
-    P, f = density(longest_contig)
-    print("Estimation of density is done")
-
-    print("MCMC is running...")
-    get_orientation([1 for i in range(len(contigs))], pairs, contigs)
-    MCMC(pairs, contigs, P, 100, n_chains=1)
-    print("Have found follow orientation:", [contigs[i].o for i in range(len(contigs))])
-
-
-    with open(path + "final.layout.txt", "w") as file:
-        sign = lambda x: "+" if x == 1 else "-"
-        file.write(",".join([contig.name + sign(contig.o) for contig in contigs]))
-    print("Result has been saved")
-
-    print("Count correctness...")
-    correct_number = np.array([contig.o == correct_contigs[id_contig[contig.name]] for contig in contigs]).sum()
-    print(f"{correct_number}/{len(contigs)} contigs were oriented correctly ({correct_number / len(contigs) * 100}%)")
+    # # Example
+    #
+    # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr1.layout.txt"
+    # path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/test_data/comp18_lens.tsv"
+    # path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs1.txt"
+    #
+    # # longest_contig
+    # pairs, contigs, id_contig, longest_contig = get_contigs_and_pairs(path_layout, path_lens, path_pairs, long_contig=True)
+    #
+    # correct_contigs = [contig.o for contig in contigs]
+    #
+    # print("Estimation of density...")
+    # P, f = density(longest_contig)
+    # print("Estimation of density is done")
+    #
+    # print("MCMC is running...")
+    # get_orientation([1 for i in range(len(contigs))], pairs, contigs)
+    # MCMC(pairs, contigs, P, 100, n_chains=1)
+    # print("Have found follow orientation:", [contigs[i].o for i in range(len(contigs))])
+    #
+    #
+    # with open(path + "final.layout.txt", "w") as file:
+    #     sign = lambda x: "+" if x == 1 else "-"
+    #     file.write(",".join([contig.name + sign(contig.o) for contig in contigs]))
+    # print("Result has been saved")
+    #
+    # print("Count correctness...")
+    # correct_number = np.array([contig.o == correct_contigs[id_contig[contig.name]] for contig in contigs]).sum()
+    # print(f"{correct_number}/{len(contigs)} contigs were oriented correctly ({correct_number / len(contigs) * 100}%)")
