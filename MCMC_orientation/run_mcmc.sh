a9a7299cd76f1543305cf1de3d4bee8a3e22b3eb
sashapff <a.ivanova@niuitmo.ru>
2021-03-21 01:52:34 +0300

    Add splitting by chromosomes

--- MCMC_orientation/run_mcmc.sh
+++ MCMC_orientation/run_mcmc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --time 01:00:00
-#SBATCH -p debug-cpu
+#SBATCH -p debug
 #SBATCH -o output.txt
 
 python main.py 1
--- preprocessing/get_contig_length.py
+++ preprocessing/get_contig_length.py
@@ -2,18 +2,41 @@ import h5py
 import numpy as np
 
 if __name__ == "__main__":
+    chr_indexes = ['MT', 'X']
+    for i in range(1, 23):
+        chr_indexes.append(str(i))
+    contigs2chr = {}
+    output_files = {}
+
+    path_layouts = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts"
+    path_output = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length"
+
     matrix_filename = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/chm13.draft_v0.9.matrix.cool"
-    output_file = open("/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig.length.txt", "w")
 
     # path = "/Users/alexandra/bioinf/mcmc/data/"
     #
     # matrix_filename = path + "mat18_50k.cool"
     # output_file = open(path + "contig_length.txt", "w")
 
+    for ind in chr_indexes:
+        if ind != "9":
+            filename = path_layouts + "/chr" + ind + ".layout.txt"
+        else:
+            filename = path_layouts + "/chr9.partial_layout.txt"
+        with open(filename, "r") as f:
+            lines = f.read().splitlines()
+        for line in lines:
+            chr_name, contigs = line.split(" ")
+            contigs = contigs.split(",")
+            for contig in contigs:
+                contigs2chr[contig[:-1]] = ind
+        output_files[ind] = open(path_output + "/contig.length." + ind + ".txt", "w")
+
     with h5py.File(matrix_filename, "r") as f:
         for i in range(len(f['chroms']['length'][:])):
-            string_to_write = (f['chroms']['name'][:][i]).decode("utf-8")  + '\t' \
-                              + str(f['chroms']['length'][:][i]) + '\n'
-            output_file.write(string_to_write)
+            contig_name = (f['chroms']['name'][:][i]).decode("utf-8")
+            string_to_write = contig_name  + '\t' + str(f['chroms']['length'][:][i]) + '\n'
+            output_files[contigs2chr[contig_name]].write(string_to_write)
 
-    output_file.close()
+    for ind in chr_indexes:
+        output_files[ind].close()
