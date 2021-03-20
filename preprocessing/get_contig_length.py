10c05dbdad7d9b891e830855aa6de9f975c6f8c4
sashapff <a.ivanova@niuitmo.ru>
2021-03-21 01:20:52 +0300

    Look for resolutions key

--- preprocessing/get_contig_length.py
+++ preprocessing/get_contig_length.py
@@ -6,9 +6,10 @@ if __name__ == "__main__":
     output_file = open("/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig.length.txt", "w")
 
     with h5py.File(matrix_filename, "r") as f:
-        for i in range(len(f['resolutions']['50000']['chroms']['length'][:])):
-            string_to_write = (f['resolutions']['50000']['chroms']['name'][:][i]).decode("utf-8")  + '\t' \
-                              + str(f['resolutions']['50000']['chroms']['length'][:][i]) + '\n'
-            output_file.write(string_to_write)
+        print(f['resolutions'].keys())
+        # for i in range(len(f['resolutions']['50000']['chroms']['length'][:])):
+        #     string_to_write = (f['resolutions']['50000']['chroms']['name'][:][i]).decode("utf-8")  + '\t' \
+        #                       + str(f['resolutions']['50000']['chroms']['length'][:][i]) + '\n'
+        #     output_file.write(string_to_write)
 
     output_file.close()
