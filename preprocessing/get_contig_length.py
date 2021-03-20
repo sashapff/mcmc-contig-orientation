9074c4e2208dda797d2b7a2c74c46580b4716ace
sashapff <a.ivanova@niuitmo.ru>
2021-03-21 01:18:33 +0300

    Add tqdm

--- preprocessing/get_contig_length.py
+++ preprocessing/get_contig_length.py
@@ -1,12 +1,13 @@
 import h5py
 import numpy as np
+from tqdm import tqdm
 
 if __name__ == "__main__":
     matrix_filename = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/chm13.draft_v0.9.matrix.cool"
     output_file = open("/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig.length.txt", "w")
 
     with h5py.File(matrix_filename, "r") as f:
-        for i in range(len(f['resolutions']['50000']['chroms']['length'][:])):
+        for i in tqdm(range(len(f['resolutions']['50000']['chroms']['length'][:]))):
             string_to_write = (f['resolutions']['50000']['chroms']['name'][:][i]).decode("utf-8")  + '\t' \
                               + str(f['resolutions']['50000']['chroms']['length'][:][i]) + '\n'
             output_file.write(string_to_write)
