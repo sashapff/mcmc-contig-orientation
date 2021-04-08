440cdc97cd0db0bff90fe98bba75b487be5ee17f
sashapff <a.ivanova@niuitmo.ru>
2021-04-08 12:17:26 +0300

    Refactor code

--- main_ordering.py
+++ main_ordering.py
@@ -3,39 +3,47 @@ from ordering.model import MCMC
 from ordering.tools import get_ordering
 from tools.prob import density, toy_density
 import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     print("Start!")
     # chr_ind = sys.argv[1]
     # Example
 
-    path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
-    # path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
+    # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
+    path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
 
-    path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
-    # path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
+    # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
+    path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
 
-    path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
-    # path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+    # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
+    path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+
+    path_to_output = "Users/alexandra/bioinf/mcmc/output"
 
     # longest_contig
-    pairs, contigs, id_contig, longest_contig, longest_contig_name = get_contigs_and_pairs(path_layout, path_lens, path_pairs, long_contig=True)
-    # pairs, contigs, id_contig, longest_contig, longest_contig_name, in_contigs = get_contigs_and_pairs(path_layout, path_lens, path_pairs,
-    #                                                                               long_contig=True,
-    #                                                                               all_contigs=True,
-    #                                                                               min_len=0)
+    # pairs, contigs, id_contig, longest_contig, longest_contig_name = get_contigs_and_pairs(path_layout, path_lens, path_pairs, long_contig=True)
+    pairs, contigs, id_contig, longest_contig, longest_contig_name, in_contigs = get_contigs_and_pairs(path_layout, path_lens, path_pairs,
+                                                                                  long_contig=True,
+                                                                                  all_contigs=True,
+                                                                                  min_len=0)
 
     correct_contigs = [contig.pos for contig in contigs]
 
     print("Estimation of density...")
-    P, f = density(longest_contig)
-    # P, f = toy_density(longest_contig)
+    # P, f = density(longest_contig)
+    P, f = toy_density(longest_contig)
     print("Estimation of density is done")
 
     print("MCMC is running...")
     get_ordering(np.random.choice(len(contigs), len(contigs), replace=False), pairs, contigs)
-    MCMC(pairs, contigs, P, 5000, n_chains=1)
+    log_likelihood_arr = MCMC(pairs, contigs, P, 5000, n_chains=1)
     print("Have found follow ordering:", [contigs[i].pos for i in range(len(contigs))])
+
+    plt.clf()
+    plt.plot('iteration number', 'log_likelihood', log_likelihood_arr)
+    plt.savefig(
+        f'{path_to_output}/plots/log_likelihood_chr{chr_ind}.png')
 
     # with open("/Users/alexandra/bioinf/mcmc/data/final1.layout.txt", "w") as file:
     #     sign = lambda x: "+" if x == 1 else "-"
--- main_orientation.py
+++ main_orientation.py
@@ -21,42 +21,46 @@ if __name__ == "__main__":
     min_contig_length = 50_000
     min_contig_length_name = '50k'
 
-    with open(f"/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/stat.txt", "w") as file:
+    # path_to_output = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}'
+    path_to_output = f'/Users/alexandra/bioinf/mcmc/output/{min_contig_length_name}'
+
+    with open(f"{path_to_output}/stat.txt", "w") as file:
         pass
 
     for chr_ind in chromosomes:
         print(f'Chromosome {chr_ind}')
-
-        path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
-
-        # if chr_ind == '9':
-        # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr9.partial_layout.txt"
-
-        path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
-        path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
-
-        # longest_contig
-        pairs, contigs, id_contig, longest_contig_pairs, longest_contig = get_contigs_and_pairs(path_layout, path_lens,
-                                                                                                path_pairs,
-                                                                                                long_contig=True,
-                                                                                                min_len=min_contig_length)
-
-        # # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
-        # path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
         #
-        # # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
-        # path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
+        # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
         #
-        # # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
-        # path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+        # # if chr_ind == '9':
+        # # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr9.partial_layout.txt"
+        #
+        # path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
+        # path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
         #
         # # longest_contig
-        # # pairs, contigs, id_contig, longest_contig, longest_contig_name = get_contigs_and_pairs(path_layout, path_lens, path_pairs,
-        #                                                                   long_contig=True)
-        # pairs, contigs, id_contig, longest_contig, longest_contig_name, in_contigs = get_contigs_and_pairs(path_layout, path_lens, path_pairs,
-        #                                                                               long_contig=True,
-        #                                                                               all_contigs=True,
-        #                                                                               min_len=0)
+        # pairs, contigs, id_contig, longest_contig_pairs, longest_contig = get_contigs_and_pairs(path_layout, path_lens,
+        #                                                                                         path_pairs,
+        #                                                                                         long_contig=True,
+        #                                                                                         min_len=min_contig_length)
+
+        # # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
+        path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
+        #
+        # # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
+        path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
+        #
+        # # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
+        path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+        #
+        # # longest_contig
+        # pairs, contigs, id_contig, longest_contig_pairs, longest_contig = get_contigs_and_pairs(path_layout, path_lens,
+        #                                                                                         path_pairs,
+        #                                                                                         long_contig=True)
+        pairs, contigs, id_contig, longest_contig_pairs, longest_contig, in_contigs = get_contigs_and_pairs(path_layout, path_lens, path_pairs,
+                                                                                      long_contig=True,
+                                                                                      all_contigs=True,
+                                                                                      min_len=0)
 
         correct_contigs = [contig.o for contig in contigs]
 
@@ -69,8 +73,8 @@ if __name__ == "__main__":
 
     print("Estimation of density...")
 
-    P, f = density(get_longest_contig(longest_contig_pairs_arr, longest_contig_arr))
-    # P, f = toy_density(longest_pairs_numpy)
+    # P, f = density(get_longest_contig(longest_contig_pairs_arr, longest_contig_arr))
+    P, f = toy_density(get_longest_contig(longest_contig_pairs_arr, longest_contig_arr))
     print("Estimation of density is done")
 
     for (j, chr_ind) in enumerate(chromosomes):
@@ -79,13 +83,11 @@ if __name__ == "__main__":
 
         print("MCMC is running...")
         get_orientation([0 for i in range(len(contigs))], pairs, contigs)
-        accuracy_arr, log_likelihood_arr = MCMC(pairs, contigs, P, number_it=200, n_chains=1,
+        accuracy_arr, log_likelihood_arr = MCMC(pairs, contigs, P, number_it=100, n_chains=1,
                                                 correct_contigs=correct_contigs)
         print("Have found follow orientation:", [contigs[i].o for i in range(len(contigs))])
 
-        with open(
-                f"/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/final_chr{chr_ind}.layout.txt",
-                "w") as file:
+        with open(f"{path_to_output}/final_chr{chr_ind}.layout.txt", "w") as file:
             sign = lambda x: "+" if x == 1 else "-"
             file.write(",".join([contig.name + sign(contig.o) for contig in contigs]))
         print("Result has been saved")
@@ -95,7 +97,7 @@ if __name__ == "__main__":
         print(
             f"{correct_number}/{len(contigs)} contigs were oriented correctly ({correct_number / len(contigs) * 100}%)")
 
-        with open(f"/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/stat.txt", "a") as file:
+        with open(f"{path_to_output}/stat.txt", "a") as file:
             file.write(f"CHROMOSOME {chr_ind}\n")
             file.write(
                 f"{correct_number}/{len(contigs)} contigs were oriented correctly ({correct_number / len(contigs) * 100}%)\n")
@@ -104,16 +106,20 @@ if __name__ == "__main__":
         contigs_total += len(contigs)
 
         plt.clf()
-        plt.plot('iteration number', 'accuracy', accuracy_arr)
+        plt.plot(accuracy_arr)
+        plt.xlabel('iteration number')
+        plt.ylabel('accuracy')
         plt.savefig(
-            f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/plots/accuracy_chr{chr_ind}.png')
+            f'{path_to_output}/plots/accuracy_chr{chr_ind}.png')
 
         plt.clf()
-        plt.plot('iteration number', 'log_likelihood', log_likelihood_arr)
+        plt.plot(log_likelihood_arr)
+        plt.xlabel('iteration number')
+        plt.ylabel('log_likelihood')
         plt.savefig(
-            f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/plots/log_likelihood_chr{chr_ind}.png')
+            f'{path_to_output}/plots/log_likelihood_chr{chr_ind}.png')
 
-    with open(f"/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}/stat.txt", "a") as file:
+    with open(f"{path_to_output}/stat.txt", "a") as file:
         file.write(f"TOTAL ACCURACY\n")
         file.write(
             f"{correct_total}/{contigs_total} contigs were oriented correctly ({correct_total / contigs_total * 100}%)\n")
