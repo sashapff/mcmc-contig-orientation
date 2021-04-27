fd8922d4cb22c3af6ca0589685aeb9b8f43cccc9
sashapff <a.ivanova@niuitmo.ru>
2021-04-27 23:53:25 +0300

    ord

--- main_ordering.py
+++ main_ordering.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     min_contig_length = 100_000
     min_contig_length_name = '100k'
 
-    path_to_output = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}'
-    # path_to_output = f'/Users/alexandra/bioinf/mcmc/output/{min_contig_length_name}'
+    # path_to_output = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/{min_contig_length_name}'
+    path_to_output = f'/Users/alexandra/bioinf/mcmc/output/{min_contig_length_name}'
 
     # chromosomes = ['1', '2', '3', '4', '5', '6', '7', '8', '10', '11', '12', '13',
     #                '14', '15', '16', '17', '18', '19', '20', '21', '22']
@@ -46,18 +46,32 @@ if __name__ == "__main__":
         print(f'Chromosome {chr_ind}')
 
         path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
+        # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
+        # path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
 
         # if chr_ind == '9':
         # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr9.partial_layout.txt"
 
         path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
-        path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
+        # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
+        # path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
 
-        # longest_contig
+        path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
+        # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
+        # path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+
+        # # longest_contig
         pairs, contigs, id_contig, longest_contig_pairs, longest_contig = get_contigs_and_pairs(path_layout, path_lens,
                                                                                                 path_pairs,
                                                                                                 long_contig=True,
                                                                                                 min_len=min_contig_length)
+
+        # pairs, contigs, id_contig, longest_contig, longest_contig_name, in_contigs = get_contigs_and_pairs(path_layout,
+        #                                                                                                    path_lens,
+        #                                                                                                    path_pairs,
+        #                                                                                                    long_contig=True,
+        #                                                                                                    all_contigs=True,
+        #                                                                                                    min_len=0)
 
         correct_contigs = [contig.pos for contig in contigs]
 
@@ -70,7 +84,7 @@ if __name__ == "__main__":
 
         shuffle_ordering(pairs, contigs, P, n_iterations=len(contigs) * 5)
 
-        log_likelihood_arr = MCMC(pairs, contigs, P, number_it=100)
+        log_likelihood_arr = MCMC(pairs, contigs, P, number_it=500)
         print("Have found follow ordering:", [contigs[i].pos for i in range(len(contigs))])
 
         plt.clf()
@@ -79,7 +93,7 @@ if __name__ == "__main__":
         plt.ylabel('log_likelihood')
         plt.legend()
         plt.title(f'Ordering log likelihood')
-        plt.savefig(f'{path_to_output}/plots/log_likelihood.png')
+        plt.savefig(f'{path_to_output}/plots_ordering/log_likelihood.png')
 
         # with open("/Users/alexandra/bioinf/mcmc/data/final1.layout.txt", "w") as file:
         #     sign = lambda x: "+" if x == 1 else "-"
