aa5cb8506f4c2cd4a8bb4d29bb936feb831c3fff
sashapff <a.ivanova@niuitmo.ru>
2021-04-28 14:52:49 +0300

    ord

--- chr3.py
+++ chr3.py
@@ -1,4 +1,6 @@
 from tools.load import check_reads, get_contigs_and_pairs
+from tools.tools import get_distance
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     chr_ind = 3
@@ -7,7 +9,32 @@ if __name__ == "__main__":
     # path_pairs = '/Users/alexandra/bioinf/mcmc/chr3/chr_pairs3.txt'
     check_reads(path_pairs, output_path, chr_ind)
 
-    # pairs, contigs, id_contig, longest_contig_pairs, longest_contig = get_contigs_and_pairs(path_layout, path_lens,
-    #                                                                                         path_pairs,
-    #                                                                                         long_contig=True,
-    #                                                                                         min_len=min_contig_length)
+    path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
+    # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
+    # path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
+
+    # if chr_ind == '9':
+    # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr9.partial_layout.txt"
+
+    path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
+    # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
+    # path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
+
+    path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
+    # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
+    # path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+
+    pairs, contigs, id_contig, longest_contig, longest_contig_name = get_contigs_and_pairs(path_layout, path_lens,
+                                                                                           path_pairs,
+                                                                                           long_contig=True)
+    distances = get_distance(pairs, contigs)
+    lengths = []
+    for (i, pair) in enumerate(pairs):
+        if pair[0] == chr_ind or pair[2] == chr_ind:
+            lengths.append(distances[i])
+
+    plt.hist(lengths)
+    plt.xlabel('distance between reads')
+    plt.ylabel(f'number of reads')
+    plt.savefig(f'{output_path}/plots_check/distances.png')
+
