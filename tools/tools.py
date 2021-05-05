37ac0808936a9fa62bc9c8e48538f5c1053028ed
sashapff <a.ivanova@niuitmo.ru>
2021-05-05 13:27:41 +0300

    default

--- tools/tools.py
+++ tools/tools.py
@@ -57,5 +57,8 @@ def log_likelihood(pairs, contigs, P):
 
 
 def filter_pairs(pairs, contig_id, left, right):
-    indx = (pairs[:, 0] == contig_id) & (pairs[:, 2] == contig_id) & (pairs[:, 1] < left) & (pairs[:, 3] >= right)
+    # indx = (pairs[:, 0] == contig_id) & (pairs[:, 2] == contig_id) & (pairs[:, 1] < left) & (pairs[:, 3] >= right)
+    indx = (pairs[:, 0] == contig_id) & (pairs[:, 2] == contig_id) \
+           & (((pairs[:, 1] < left) & (pairs[:, 3] >= right)) | ((pairs[:, 1] < left) & (pairs[:, 3] < left))
+              | ((pairs[:, 1] >= right) & (pairs[:, 3] >= right)))
     return pairs[indx]
