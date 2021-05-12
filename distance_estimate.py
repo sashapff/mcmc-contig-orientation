7e703d24ef4a9965aad619fd74ce2cd4912d4131
sashapff <a.ivanova@niuitmo.ru>
2021-05-12 15:48:53 +0300

    default

--- distance_estimate.py
+++ distance_estimate.py
@@ -2,6 +2,8 @@ from tools.load import get_contigs_and_pairs
 from tools.prob import density, toy_density
 from tools.tools import get_longest_contig, filter_pairs, log_likelihood, get_distance_one_contig
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import quad
 
 if __name__ == "__main__":
     print("Start!")
@@ -36,10 +38,10 @@ if __name__ == "__main__":
     ind = (pairs[:, 1] >= pairs[:, 3])
     pairs[ind, 1], pairs[ind, 3] = pairs[ind, 3], pairs[ind, 1]
 
-    d = 1000
+    D = 1000
     # d = 5
-    left = longest_contig.length // 2 - d // 2
-    right = left + d
+    left = longest_contig.length // 2 - D // 2
+    right = left + D
 
     filtered_pairs = filter_pairs(pairs, id_contig[longest_contig.name], left, right)
     filtered_pairs[:, 3] -= right
@@ -47,11 +49,13 @@ if __name__ == "__main__":
     log_likelihood_arr = []
     log_likelihood_range = [1, 10, 100, 1000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000]
 
-    for i in log_likelihood_range:
-        filtered_pairs[:, 6] = i
-        ll = P(get_distance_one_contig(filtered_pairs, left)).sum()
+    for d in log_likelihood_range:
+        filtered_pairs[:, 6] = d
+        coeff = quad(np.exp(P), d, 10**10)
+        print(f'd={d}, coeff={coeff}')
+        ll = P(get_distance_one_contig(filtered_pairs, left)).sum() * coeff
         log_likelihood_arr.append(ll)
-        print(f'likelihood for d={i} is {ll}')
+        print(f'likelihood for d={d} is {ll}')
 
     plt.plot(log_likelihood_range, log_likelihood_arr)
     plt.xlabel('d, distance estimate')
