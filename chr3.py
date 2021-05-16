ae20d65bb449e6317158488226df8aa37c7a962d
sashapff <a.ivanova@niuitmo.ru>
2021-05-16 12:05:05 +0300

    default

--- chr3.py
+++ chr3.py
@@ -1,35 +1,33 @@
 import numpy as np
 from orientation.tools import get_orientation
 from tools.load import check_reads, get_contigs_and_pairs
-from tools.prob import density
+from tools.prob import density, toy_density
 from tools.tools import get_distance, log_likelihood
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     chr_ind = '3'
-    path_pairs = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs{chr_ind}.txt'
-    output_path = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/'
-    # path_pairs = '/Users/alexandra/bioinf/mcmc/chr3/chr_pairs3.txt'
+    # path_pairs = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs{chr_ind}.txt'
+    # output_path = f'/lustre/groups/cbi/Users/aeliseev/aivanova/data/'
+    output_path = '/Users/alexandra/bioinf/mcmc/simulate/'
 
     # check_reads(path_pairs, output_path, chr_ind)
 
-    path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
+    # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr" + chr_ind + ".layout.txt"
     # path_layout = "/Users/alexandra/bioinf/mcmc/data/chr1.layout.txt"
-    # path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
+    path_layout = "/Users/alexandra/bioinf/mcmc/data/simulation.layout.txt"
 
-    # if chr_ind == '9':
-    # path_layout = "/GWSPH/groups/cbi/Users/pavdeyev/HiCProject/layouts/chr9.partial_layout.txt"
-
-    path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
+    # path_lens = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/contig_length/contig.length." + chr_ind + ".txt"
     # path_lens = "/Users/alexandra/bioinf/mcmc/data/comp18_lens.tsv"
-    # path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
+    path_lens = "/Users/alexandra/bioinf/mcmc/data/simulation.lens.tsv"
 
-    path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
+    # path_pairs = "/lustre/groups/cbi/Users/aeliseev/aivanova/data/pairs/chr_pairs" + chr_ind + ".txt"
     # path_pairs = "/Users/alexandra/bioinf/mcmc/data/pairs18.txt"
-    # path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
+    path_pairs = "/Users/alexandra/bioinf/mcmc/data/simulation.pairs.txt"
 
     pairs, contigs, id_contig, longest_contig, longest_contig_name = get_contigs_and_pairs(path_layout, path_lens,
                                                                                            path_pairs,
+                                                                                           min_len=0,
                                                                                            long_contig=True)
     print(f"Analyse {len(contigs)} contigs..")
 
@@ -37,29 +35,30 @@ if __name__ == "__main__":
 
     correct_number = np.array([contig.o == correct_contigs[id_contig[contig.name]] for contig in contigs]).sum()
 
-    P = density(longest_contig)
+    # P, f = density(longest_contig)
+    P = toy_density(longest_contig)
 
-    for i in range(2):
-        for j in range(2):
-            get_orientation([i, j], pairs, contigs)
-            correct_number = np.array([contig.o == correct_contigs[id_contig[contig.name]] for contig in contigs]).sum()
-            print(f"{[contig.o for contig in contigs]}:")
-            print(f'\tcorrectness: {correct_number}/{len(contigs)} ({correct_number / len(contigs) * 100}%)')
-            print(f'\tlog_likelihood: {log_likelihood(pairs, contigs, P)}')
-
-            distances = get_distance(pairs, contigs)
-            lengths = []
-            for (k, pair) in enumerate(pairs):
-                if distances[k] < 100_000:
-                    lengths.append(distances[k])
-
-            plt.hist(lengths)
-            print(f'\tdistance mean: {np.array(lengths).mean()}')
-            plt.title(f'Orientation {[contig.o for contig in contigs]}, {correct_number / len(contigs) * 100}%')
-            plt.xlabel('distance between reads')
-            plt.ylabel(f'number of reads')
-            plt.savefig(f'{output_path}/plots_check/distances{[contig.o for contig in contigs]}.png')
-            plt.clf()
+    # for i in range(2):
+    #     for j in range(2):
+    #         get_orientation([i, j], pairs, contigs)
+    #         correct_number = np.array([contig.o == correct_contigs[id_contig[contig.name]] for contig in contigs]).sum()
+    #         print(f"{[contig.o for contig in contigs]}:")
+    #         print(f'\tcorrectness: {correct_number}/{len(contigs)} ({correct_number / len(contigs) * 100}%)')
+    #         print(f'\tlog_likelihood: {log_likelihood(pairs, contigs, P)}')
+    #
+    #         distances = get_distance(pairs, contigs)
+    #         lengths = []
+    #         for (k, pair) in enumerate(pairs):
+    #             if distances[k] < 100_000:
+    #                 lengths.append(distances[k])
+    #
+    #         plt.hist(lengths)
+    #         print(f'\tdistance mean: {np.array(lengths).mean()}')
+    #         plt.title(f'Orientation {[contig.o for contig in contigs]}, {correct_number / len(contigs) * 100}%')
+    #         plt.xlabel('distance between reads')
+    #         plt.ylabel(f'number of reads')
+    #         plt.savefig(f'{output_path}/plots_check/distances{[contig.o for contig in contigs]}.png')
+    #         plt.clf()
 
     min_left = 10 ** 10
     max_left = 0
@@ -84,12 +83,12 @@ if __name__ == "__main__":
     plt.title(f'Orientation {[contig.o for contig in contigs]}, {correct_number / len(contigs) * 100}%')
     plt.xlabel('read position in the first contig')
     plt.ylabel(f'number of reads')
-    plt.savefig(f'{output_path}/plots_check/positions_0.png')
+    plt.savefig(f'{output_path}/positions_0.png')
     plt.clf()
 
     plt.hist(pos_right)
     plt.title(f'Orientation {[contig.o for contig in contigs]}, {correct_number / len(contigs) * 100}%')
     plt.xlabel('read position in the second contig')
     plt.ylabel(f'number of reads')
-    plt.savefig(f'{output_path}/plots_check/positions_1.png')
+    plt.savefig(f'{output_path}/positions_1.png')
     plt.clf()
